978c0ef17201c9d96b5109021b8066851b4b0c46
vien pham <vienpham2019@gmail.com>
2019-12-18 03:44:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,7 @@ end
 def initial_round
   
   # code #initial_round here
-  card_1 = deal_card
-  card_2 = deal_card
-  card_total = (card_1 + card_2)
+  card_total = (deal_card + deal_card)
   display_card_total(card_total)
 end
 
