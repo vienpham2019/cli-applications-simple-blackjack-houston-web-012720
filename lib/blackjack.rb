ad584067ca7b607bb9ffb3b8e3213973a43d3007
vien pham <vienpham2019@gmail.com>
2019-12-18 03:43:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,7 +28,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  puts "Sorry, you hit #{card_total}.Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
