10dadd724e2e39f29bb1f6ef61cb72962233a2ca
vien pham <vienpham2019@gmail.com>
2019-12-18 03:32:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -9,8 +9,9 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total()
+def display_card_total(n)
   # code #display_card_total here
+  cards_total = n
   puts "Your cards add up to #{card_total}"
 end
 
