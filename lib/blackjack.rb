4a360225090a478ac6e3255246b49bba87eb4877
vien pham <vienpham2019@gmail.com>
2019-12-18 04:08:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,7 @@ end
 def hit?(card_total)
   # code hit? here
   prompt_user
-  get_user_input = input
+  input = get_user_input
   if input == "h"
     card_total += deal_card
   elsif input == "s"
