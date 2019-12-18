9731ae58e4182fbc0fa2e9f3e9a9a54976a14f00
vien pham <vienpham2019@gmail.com>
2019-12-18 04:28:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ end
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
-  get_user_input
+  # get_user_input
 end
 
 #####################################################
